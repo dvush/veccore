99c316dc088f9f8af513500af3ef271883692000
Soon Yung Jun <syjun@tev.fnal.gov>
2018-03-30 17:43:08 -0500

    remove the scalar specialization for Philox4x32round and fix Mulhilo32 taking into account the size factor of Unit32_v/Unit64_v

--- include/VecRng/Philox.h
+++ include/VecRng/Philox.h
@@ -136,7 +136,7 @@ private:
                                                 typename BackendT::UInt32_v b, 
                                                 typename BackendT::UInt32_v *hip);
 
-  VECCORE_ATT_HOST_DEVICE inline void Philox4x32bumpkey(R123::array2_t<BackendT> key);
+  VECCORE_ATT_HOST_DEVICE inline void Philox4x32bumpkey(R123::array_t<BackendT,2> key);
 
   VECCORE_ATT_HOST_DEVICE void Philox4x32round(R123::array_t<BackendT,4> crt, 
   					       R123::array_t<BackendT,2> key);
@@ -407,20 +407,26 @@ typename BackendT::UInt32_v Philox<BackendT>::Mulhilo32(typename BackendT::UInt3
   using UInt32_v = typename BackendT::UInt32_v;                              
   using UInt64_v = typename BackendT::UInt64_v;                              
 
-  //  UInt64_v product = ((UInt64_v)a)*((UInt64_v)b);        
-  //  *hip = product >> 32;                                  
-  //  return (UInt32_v)product; 
-
-  //  @@@syj - we do not have Vc::UInt64_v for AVX/AVX2
-  //  use sclar for now
-
   UInt64_v product;
   UInt32_v result;
 
-  for(size_t i = 0 ; i < VectorSize<UInt32_v>() ; ++i) { 
-    product[i] = ((uint64_t)a[i])*((uint64_t)b[i]);
-    (*hip)[i] = product[i] >> 32;                                  
-    result[i] = ((uint32_t)product[i]);
+  // UInt64_v product = ((UInt64_v)a)*((UInt64_v)b);        
+  // *hip = product >> 32;                                  
+  // return (UInt32_v)product; 
+
+  // no conversion between UInt64_v and UInt32_v: size(UInt32_v)/size(UInt64_v) = 2
+  // use the scalar-loop: vector->scalar bottleneck 
+
+  size_t ibase = 0;
+  size_t vsize64 = VectorSize<UInt64_v>();
+
+  for(size_t i = 0 ; i < VectorSize<UInt32_v>()/vsize64 ; ++i) { 
+    for(size_t j = 0 ; j < vsize64 ; ++j) { 
+      product[j] = ((uint64_t)a[ibase+j])*((uint64_t)b[ibase+j]);
+      (*hip)[ibase+j] = product[j] >> 32;                                  
+      result[ibase+j] = ((uint32_t)product[j]);
+    }
+    ibase += vsize64;
   }
   return result; 
 }
@@ -442,30 +448,11 @@ ScalarBackend::UInt32_v Philox<ScalarBackend>::Mulhilo32(uint32_t a, uint32_t b,
 
 template <class BackendT>
 inline
-VECCORE_ATT_HOST_DEVICE void Philox<BackendT>::Philox4x32bumpkey(R123::array2_t<BackendT> key)
+VECCORE_ATT_HOST_DEVICE void Philox<BackendT>::Philox4x32bumpkey(R123::array_t<BackendT,2> key)
 {
   using UInt32_v = typename BackendT::UInt32_v;                              
   key[0] += (UInt32_v)R123::PHILOX_N4_W32_0;                                       
   key[1] += (UInt32_v)R123::PHILOX_N4_W32_1;                                       
-}  
-
-template <>
-inline
-VECCORE_ATT_HOST_DEVICE void 
-Philox<ScalarBackend>::Philox4x32round(R123::array_t<ScalarBackend,4> ctr, 
-                                       R123::array_t<ScalarBackend,2> key)
-{
-  uint32_t hi0;                                                        
-  uint32_t hi1;                                                        
-
-  uint32_t lo0 = Mulhilo32((uint32_t)R123::PHILOX_M4_W32_0, ctr[0], &hi0);            
-  uint32_t lo1 = Mulhilo32((uint32_t)R123::PHILOX_M4_W32_1, ctr[2], &hi1);            
-
-  //  UInt32_v out[4] = {hi1^ctr[1]^key[0], lo1, hi0^ctr[3]^key[1], lo0};    
-  ctr[0] = hi1^ctr[1]^key[0];
-  ctr[1] = lo1;
-  ctr[2] = hi0^ctr[3]^key[1];
-  ctr[3] = lo0;
 }  
 
 template <class BackendT>
@@ -482,13 +469,13 @@ Philox<BackendT>::Philox4x32round(R123::array_t<BackendT,4> ctr,
   UInt32_v lo0 = Mulhilo32((UInt32_v)R123::PHILOX_M4_W32_0, ctr[0], &hi0);            
   UInt32_v lo1 = Mulhilo32((UInt32_v)R123::PHILOX_M4_W32_1, ctr[2], &hi1);            
 
-  //  UInt32_v out[4] = {hi1^ctr[1]^key[0], lo1, hi0^ctr[3]^key[1], lo0};    
-    ctr[0] = hi1^ctr[1]^key[0];
-    ctr[1] = lo1;
-    ctr[2] = hi0^ctr[3]^key[1];
-    ctr[3] = lo0;
+  // a significant performance bottleneck for evaluating ctr[0] for both 
+  // scalar and vector backends, especially in the assignment op= (to hi1)
+  ctr[0] = hi1^ctr[1]^key[0]; 
+  ctr[1] = lo1;
+  ctr[2] = hi0^ctr[3]^key[1];
+  ctr[3] = lo0;
 }  
-
 
 template <class BackendT>
 VECCORE_ATT_HOST_DEVICE 
